aba8704637d29ccd487b63e9d2e2b69eb6274a22
JnerdQ <julyanyto@gmail.com>
2024-06-09 20:23:44 -0500

    changes in auth, CORS added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-# main.py
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from app.database import SessionLocal, engine 
 import app.models as models
@@ -10,7 +10,18 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app = FastAPI()
+# Configuración de CORS
+origins = [
+    "http://localhost:5173",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency db session
 def get_db():
@@ -24,14 +35,13 @@ def get_db():
 def read_root():
     return {"message": "Welcome to the Employee API"}
 
-#Create Employee
+# Create Employee
 @app.post("/employee/", response_model=schemas.Employee, tags=["employee"])
 def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
     db_employee = crud.get_employee_by_email(db, email=employee.email)
     if db_employee:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_employee(db=db, employee=employee)
-
 
 # Get all employees
 @app.get("/employee/", response_model=list[schemas.Employee], tags=["employee"])
@@ -47,16 +57,15 @@ def read_employee(employee_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Employee not found")
     return db_employee
 
-
-# authenticate employee
-@app.post("/employee/authenticate", response_model=schemas.Employee, tags=["employee"])
+# Authenticate employee
+@app.post("/employee/authenticate", tags=["employee"])
 def authenticate_employee(auth_data: schemas.EmployeeAuthenticate, db: Session = Depends(get_db)):
     db_employee = crud.authenticate_employee(db, email=auth_data.email, password=auth_data.password)
     if db_employee is None:
         raise HTTPException(status_code=400, detail="Invalid credentials")
-    return db_employee
+    return {"message": "Employee authenticated successfully"}
 
-#delete employee by id
+# Delete employee by id
 @app.delete("/employee/{employee_id}", tags=["employee"])
 def delete_employee(employee_id: int, db: Session = Depends(get_db)):
     db_employee = crud.get_employee(db, employee_id=employee_id)
@@ -65,10 +74,8 @@ def delete_employee(employee_id: int, db: Session = Depends(get_db)):
     crud.delete_employee(db, employee_id=employee_id)
     return {"message": "Employee deleted successfully"}
 
-"""
-# delete all employees
-@app.delete("/employees/", tags=["employee"])
+# Delete all employees
+@app.delete("/employee/", tags=["employee"])
 def delete_all_employees(db: Session = Depends(get_db)):
     crud.delete_all_employees(db)
     return {"message": "All employees have been deleted"}
-"""
